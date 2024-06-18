14f5f6a017621bfaced9a7f6988a41949c46851a
Vladislav Kravchuk <111505135+flar1ngan@users.noreply.github.com>
2024-06-18 09:52:52 +0300

    Update dark-mode-toggle.js

--- dark-mode-toggle.js
+++ dark-mode-toggle.js
@@ -37,7 +37,7 @@ function colorModeToggle() {
     if (lightValue.length) {
       if (!darkValue.length) darkValue = lightValue;
       lightColors[`--color--${item}`] = lightValue;
-      darkColors[`--color--${item}`] = darkValue;
+      darkColors[`--dark--${item}`] = darkValue;
     }
   });
 
@@ -60,17 +60,28 @@ function colorModeToggle() {
     }
   }
 
+  function toggleImages(isDarkMode) {
+    var imagesToToggle = document.querySelectorAll('[data-toggle-image]');
+    imagesToToggle.forEach(function(image) {
+      image.style.filter = isDarkMode ? 'invert(100%)' : 'invert(0%)';
+    });
+  }
+
   function goDark(dark, animate) {
     if (dark) {
       localStorage.setItem("dark-mode", "true");
+      localStorage.setItem('isInverted', 'true');
       htmlElement.classList.add("dark-mode");
       setColors(darkColors, animate);
       togglePressed = "true";
+      toggleImages(true);
     } else {
       localStorage.setItem("dark-mode", "false");
+      localStorage.setItem('isInverted', 'false');
       htmlElement.classList.remove("dark-mode");
       setColors(lightColors, animate);
       togglePressed = "false";
+      toggleImages(false);
     }
     if (typeof toggleEl !== "undefined") {
       toggleEl.forEach(function (element) {
@@ -110,3 +121,26 @@ function colorModeToggle() {
   });
 }
 colorModeToggle();
+
+document.addEventListener("DOMContentLoaded", function() {
+  function toggleImages(isDarkMode) {
+    var imagesToToggle = document.querySelectorAll('[data-toggle-image]');
+    imagesToToggle.forEach(function(image) {
+      image.style.filter = isDarkMode ? 'invert(100%)' : 'invert(0%)';
+    });
+  }
+
+  var storagePreference = localStorage.getItem("dark-mode");
+  var isDarkMode = storagePreference === 'true';
+  toggleImages(isDarkMode);
+
+  const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.addedNodes.length > 0) {
+        toggleImages(isDarkMode);
+      }
+    });
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+});
